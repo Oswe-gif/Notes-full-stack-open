024a74ba74ee3a1b637d29c5e93b4798deb31942
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-12-01 12:30:45 -0500

    Add URIs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Note from './components/Note'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import noteService from './services/notes'
+
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -12,6 +14,21 @@ const App = () => {
 
   //const promise = axios.get('http://localhost:3001/notes')
   //console.log("la promesa bro", promise)
+  const toggleImportanceOf = id => {
+    //const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    noteService.update(id, changedNote).then(returnedNote  => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    }).catch(error =>console.log('fallo')).then(console.log('otro then porque puedo agregar tantos then como quiera para un mismo catch')).catch(error => {
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })//aqui no usamos el response.data porque desde el service ya lo implemente
+    //coloco un catch en el then para atrapar el error o al final de todo para atrapar el error de alguno de ellos, aunque lo más común es colocarlo al final
+  }
+
 
   const hook = () => {
     /*console.log('effect')
@@ -28,8 +45,12 @@ const App = () => {
         setNotes(response.data)
       }
     
-      const promise = axios.get('http://localhost:3001/notes')
-      promise.then(eventHandler)
+      //const promise = axios.get('http://localhost:3001/notes')
+      //promise.then(eventHandler)
+
+      noteService.getAll().then(data =>{
+        setNotes(data);
+      });
   }
   //puedes meter la función directo en el hook en lugar de definir eso como una variable.
   useEffect(hook, [])
@@ -43,11 +64,17 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
+      //id: notes.length + 1, lo comento ya que es mejor dejar que el servidor genere identificadores para nuestros recursos
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    noteService
+    .create(noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
+
   }
   const handleNoteChange = (event) => {
     console.log(event.target.value)
@@ -68,7 +95,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
       <form onSubmit={addNote}>
